b4c70955501d6e1e5d2221266ef901d99eeac737
ifeanyiOkey <ifeanyi.okey@gmail.com>
2024-09-18 16:18:42 +0100

    fix: api result

--- routes/api.js
+++ routes/api.js
@@ -12,9 +12,9 @@ module.exports = function (app) {
       console.log(locale);
       if (!text) return res.json({ error: 'No text to translate' });
       if (!text || !locale) return res.json({ error: 'Required field(s) missing' });
-      if (locale !== 'american-to-british' || locale !== 'british-to-american')
-        return res.json({ error: 'Invalid value for locale field' });
+      // if (locale !== 'american-to-british' || locale !== 'british-to-american')
+      //   return res.json({ error: 'Invalid value for locale field' });
       const translation = translator.translate(text, locale);
-      return res.json({ translation });
+      return res.json({ 'text': text, 'translation': translation });
     });
 };
