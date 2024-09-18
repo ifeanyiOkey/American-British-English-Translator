d67a5528957d303e66ee74d8cddc323f1ae65c68
ifeanyiOkey <ifeanyi.okey@gmail.com>
2024-09-18 18:25:46 +0100

    verify: missing property

--- routes/api.js
+++ routes/api.js
@@ -10,12 +10,15 @@ module.exports = function (app) {
     .post((req, res) => {
       const { text, locale } = req.body;
       console.log(req.body);
-      if (!text) return res.json({ error: 'No text to translate' });
-      if (!locale)
-        return res.json({ error: 'Invalid value for locale field' });
-      if (!Object.hasOwn(req.body, 'text') || !Object.hasOwn(req.body, 'locale'))
+      
+      if (Object.hasOwn(req.body, 'text') && Object.hasOwn(req.body, 'locale')) {
+        if (!text) return res.json({ error: 'No text to translate' });
+        if (locale !== 'american-to-british' && locale !== 'british-to-american')
+          return res.json({ error: 'Invalid value for locale field' });
+        const translation = translator.translate(text, locale);
+        return res.json({ 'text': text, 'translation': translation });
+      } else {
         return res.json({ error: 'Required field(s) missing' })
-      const translation = translator.translate(text, locale);
-      return res.json({ 'text': text, 'translation': translation });
+      }
     });
 };
